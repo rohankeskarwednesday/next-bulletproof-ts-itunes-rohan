a625fc2622777c82cf1bcf858319587b6fd389cc
Rohan Keskar <rohan.keskar@wednesday.is>
2022-08-19 18:28:04 +0530

    fix(frontend): made changes to postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -16,7 +16,10 @@ module.exports = {
     [
       "@fullhuman/postcss-purgecss",
       {
-        content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+        content: [
+          "./**/*.{js,jsx,ts,tsx}",
+          // "./components/**/*.{js,jsx,ts,tsx}"
+        ],
         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
         safelist: ["html", "body"],
       },
