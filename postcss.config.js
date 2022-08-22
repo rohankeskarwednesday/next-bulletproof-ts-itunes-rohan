2d3f10df5a556dbdbf7ec2a3e91202404027eee1
Rohan Keskar <rohan.keskar@wednesday.is>
2022-08-22 11:44:24 +0530

    fix(frontend): removed purge css

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    [
+      "@fullhuman/postcss-purgecss",
+      process.env.NODE_ENV === "production"
+        ? {
+            // the paths to all template files
+            content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+            // function used to extract class names from the templates
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          }
+        : false,
+    ],
+  ],
+};
