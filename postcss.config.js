aee78078eee63a9fb3ad3d164546790d1ec3e04e
Rohan Keskar <rohan.keskar@wednesday.is>
2022-08-22 11:03:27 +0530

    fix(frontend): made changes to postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,27 @@
 module.exports = {
   plugins: [
-    "postcss-flexbugs-fixes",
-    [
-      "postcss-preset-env",
-      {
-        autoprefixer: {
-          flexbox: "no-2009",
-        },
-        stage: 3,
-        features: {
-          "custom-properties": false,
-        },
-      },
-    ],
+    // "postcss-flexbugs-fixes",
+    // [
+    //   "postcss-preset-env",
+    //   {
+    //     autoprefixer: {
+    //       flexbox: "no-2009",
+    //     },
+    //     stage: 3,
+    //     features: {
+    //       "custom-properties": false,
+    //     },
+    //   },
+    // ],
     [
       "@fullhuman/postcss-purgecss",
       {
-        content: [
-          "./containers/**/*.{js,jsx,ts,tsx}",
-          "./features/**/*.{js,jsx,ts,tsx}",
-          "./pages/**/*.{js,jsx,ts,tsx}",
-        ],
+        // content: [
+        //   "./containers/**/*.{js,jsx,ts,tsx}",
+        //   "./features/**/*.{js,jsx,ts,tsx}",
+        //   "./pages/**/*.{js,jsx,ts,tsx}",
+        // ],
+        content: ["./**/*.{js,jsx,ts,tsx}"],
         // defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
         defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
         safelist: ["html", "body"],
